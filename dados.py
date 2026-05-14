61adfc3f077a8971469bb0a6d89b318fe0ffdd37
ARES55Marinho <multiagranel2024@gmail.com>
2026-05-14 11:02:03 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,18 +1,30 @@
-livro_titulo = input('digite o nome do seu novo livro ')
-livro_autor = input('digite o nome do autor do seu novo livro ')
-livro_ano = int(input('digite o ano que foi lançado o livro '))
-livro_genero = input('digite o gênero do livro ')
+def continuar():
+        livro_titulo = input('digite o nome do seu novo livro ')
+        livro_autor = input('digite o nome do autor do seu novo livro ')
+        livro_ano = int(input('digite o ano que foi lançado o livro '))
+        livro_genero = input('digite o gênero do livro ')
 
 
 
-Livro = {
-    "Título": livro_titulo,
-    "Autor": livro_autor,
-    "Ano de publicação": livro_ano,
-    "Gênero": livro_genero
-}
+        Livro = {
+        "Título": livro_titulo,
+        "Autor": livro_autor,
+        "Ano de publicação": livro_ano,
+        "Gênero": livro_genero
+        }
 
-print("\n===== Livro =====")
+        print("\n===== Livro =====")
 
-for chave, valor in Livro.items():
-    print(f"{chave}: {valor}")
+        for chave, valor in Livro.items():
+            print(f"{chave}: {valor}")
+
+
+while True:
+    adicionar = input('Você quer adicionar mais algum livro? (S/N) ').upper()
+    if adicionar == 'N':
+        print('Aproveite seus novos livros')
+        break
+    elif adicionar == "S":
+        continuar()
+    else:
+        print('Por favor digite apenas S ou N')
