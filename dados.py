803be709c522aab9d43098f807c16ab60325b90a
ARES55Marinho <multiagranel2024@gmail.com>
2026-05-14 09:21:29 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,1 +1,20 @@
-livro_nome = input('digite o nome do seu novo livro')
+livro_titulo = input('digite o nome do seu novo livro ')
+livro_autor = input('digite o nome do autor do seu novo livro ')
+livro_ano = int(input('digite o ano que foi lançado o livro '))
+livro_genero = input('digite o gênero do livro ')
+
+
+
+Livro = {
+    "Título": livro_titulo,
+    "Autor": livro_autor,
+    "Ano de publicação": livro_ano,
+    "Gênero": livro_genero
+}
+
+print("\n===== Livro =====")
+
+print(f"Título:{livro_titulo}")
+print(f"Autor:{livro_autor}")  
+print(f"Ano de publicação:{livro_ano}")  
+print(f"Gênero:{livro_genero}")  
